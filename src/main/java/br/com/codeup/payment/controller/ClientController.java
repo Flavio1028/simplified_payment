504003d77672f3554f175f489b55614b2f729d0e
Flávio Rocha <flavio-10_28@hotmail.com>
2024-04-29 15:54:52 -0300

    Configuração Swagger

--- src/main/java/br/com/codeup/payment/controller/ClientController.java
+++ src/main/java/br/com/codeup/payment/controller/ClientController.java
@@ -2,8 +2,13 @@ package br.com.codeup.payment.controller;
 
 import br.com.codeup.payment.model.ClientDTO;
 import br.com.codeup.payment.model.ClientSaveDTO;
+import br.com.codeup.payment.model.ErrorDTO;
 import br.com.codeup.payment.model.SuccessDTO;
 import br.com.codeup.payment.service.ClientService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,26 +26,61 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = SuccessDTO.class))}),
+            @ApiResponse(responseCode = "400", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))}),
+            @ApiResponse(responseCode = "500", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))})})
     @PostMapping
     public ResponseEntity<SuccessDTO> save(@RequestBody ClientSaveDTO clientSaveDTO) {
         return ResponseEntity.status(201).body(this.clientService.save(clientSaveDTO));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Page.class))}),
+            @ApiResponse(responseCode = "400", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))}),
+            @ApiResponse(responseCode = "500", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))})})
     @GetMapping
     public ResponseEntity<Page<ClientDTO>> getAll(Pageable page) {
         return ResponseEntity.ok(this.clientService.getAll(page));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ClientDTO.class))}),
+            @ApiResponse(responseCode = "400", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))}),
+            @ApiResponse(responseCode = "500", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))})})
     @GetMapping("/{id}")
     public ResponseEntity<ClientDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(this.clientService.getById(id));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = SuccessDTO.class))}),
+            @ApiResponse(responseCode = "400", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))}),
+            @ApiResponse(responseCode = "500", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))})})
     @PutMapping("/{id}")
     public ResponseEntity<SuccessDTO> update(@PathVariable Long id, @RequestBody ClientSaveDTO clientSaveDTO) {
         return ResponseEntity.ok(this.clientService.update(id, clientSaveDTO));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = SuccessDTO.class))}),
+            @ApiResponse(responseCode = "400", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))}),
+            @ApiResponse(responseCode = "500", content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class))})})
     @DeleteMapping("/{id}")
     public ResponseEntity<SuccessDTO> delete(@PathVariable Long id) {
         return ResponseEntity.ok(this.clientService.delete(id));
--- src/main/java/br/com/codeup/payment/model/ClientSaveDTO.java
+++ src/main/java/br/com/codeup/payment/model/ClientSaveDTO.java
@@ -1,5 +1,6 @@
 package br.com.codeup.payment.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,22 +20,28 @@ public class ClientSaveDTO {
     private static final String BLANK_MESSAGE = "não pode ser nulo, vazio ou em branco.";
     private static final String NOT_NULL_MESSAGE = "Não pode ser nulo.";
 
+    @Schema(description = "Nome do cliente", example = "Fulano Teste")
     @NotBlank(message = BLANK_MESSAGE)
     private String name;
 
+    @Schema(description = "E-mail do cliente", example = "teste@teste.com.br")
     @NotBlank(message = BLANK_MESSAGE)
     @Email(message = "é inválido.")
     private String email;
 
+    @Schema(description = "Númedo do documento, podendo ser CPF/CNPJ", example = "12345678911")
     @NotBlank(message = BLANK_MESSAGE)
     private String documentNumber;
 
+    @Schema(description = "Senha do cliente", example = "ABC123")
     @NotBlank(message = BLANK_MESSAGE)
     private String password;
 
+    @Schema(description = "Valor do saldo", example = "1500.0")
     @NotNull(message = NOT_NULL_MESSAGE)
     private BigDecimal balance;
 
+    @Schema(description = "Tipo de Cliente", example = "C", allowableValues = {"C - Cliente", "L - Lojista"})
     @NotNull(message = NOT_NULL_MESSAGE)
     private String clientType;
 
